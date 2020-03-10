39bad2432764e43a1a927681cd7feac9ad663903
Roberto Zanchi <r.zanchi@gmail.com>
2020-03-09 21:11:22 -0400

    started lesson 4 exercises

---
+++ lesson-4/selectFruit.js
@@ -0,0 +1,17 @@
+let produce = {
+  apple: "Fruit",
+  carrot: "Vegetable",
+  pear: "Fruit",
+  broccoli: "Vegetable"
+};
+
+let fruit = {};
+
+for (let key in produce) {
+  if (produce[key] === "Fruit") {
+    fruit[key] = "Fruit";
+  }
+}
+
+console.log(fruit);
+return fruit;
