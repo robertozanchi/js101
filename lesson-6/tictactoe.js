f873d9828978b4efe74cb2a78892471684967814
Roberto Zanchi <r.zanchi@gmail.com>
2020-03-31 18:05:13 -0400

    started tictactoe

---
+++ lesson-6/tictactoe.js
@@ -0,0 +1,15 @@
+function displayBoard(board) {
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
+  console.log("     |     |");
+  console.log("");
+}
