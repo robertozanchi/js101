12e02b851caac5a49af14b39769f0ad773d29754
Roberto Zanchi <r.zanchi@gmail.com>
2020-04-04 11:22:45 -0400

    continue tictactoe

--- lesson-6/tictactoe.js
+++ lesson-6/tictactoe.js
@@ -8,6 +8,10 @@ function prompt(message) {
 }
 
 function displayBoard(board) {
+  console.clear();
+
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
+
   console.log("");
   console.log("     |     |");
   console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
@@ -58,12 +62,74 @@ function computerChoosesSquare(board) {
   board[square] = COMPUTER_MARKER;
 }
 
+function boardFull(board) {
+  return emptySquares(board).length === 0;
+}
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9], // rows
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9], // columns
+    [1, 5, 9],
+    [3, 5, 7] // diagonals
+  ];
+
+  for (let line = 0; line < winningLines.length; line++) {
+    let [sq1, sq2, sq3] = winningLines[line];
+
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      return "Player";
+    } else if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      return "Computer";
+    }
+  }
+
+  return null;
+}
+
+function someoneWon(board) {
+  return !!detectWinner(board);
+}
+
 // At bottom of board
 
-let board = initializeBoard();
-displayBoard(board);
+while (true) {
+  let board = initializeBoard();
+  //displayBoard(board);
 
-playerChoosesSquare(board);
-computerChoosesSquare(board);
+  while (true) {
+    displayBoard(board);
 
-displayBoard(board);
+    playerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+
+    computerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+  }
+
+  displayBoard(board);
+
+  if (someoneWon(board)) {
+    prompt(`${detectWinner(board)} won!`);
+  } else {
+    prompt("It's a tie!");
+  }
+
+  prompt("Play again? (y or n)");
+  let answer = readline.question().toLowerCase()[0];
+  if (answer !== "y") break;
+}
+
+prompt("Thanks for playing Tic Tac Toe!");
